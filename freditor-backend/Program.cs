7e5a16624e901de268c571351146280c64bf4780
tdevelop12 <pawel.turon1@gmail.com>
2022-04-12 10:22:35 +0200

    Update ports
    Update ports

--- freditor-backend/Program.cs
+++ freditor-backend/Program.cs
@@ -1,11 +1,6 @@
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.IO;
 
 namespace FreditorBackend
 {
@@ -13,14 +8,15 @@ namespace FreditorBackend
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
-        }
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseUrls("http://*:5000")
+                .UseIISIntegration()
+                .UseStartup<Startup>()
+                .Build();
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>().UseUrls("http://localhost:4200");
-                });
+            host.Run();
+        }
     }
 }
