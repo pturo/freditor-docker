f5f5670274d4ef5720e04b673fa2bcff8730fac9
tdevelop12 <pawel.turon1@gmail.com>
2022-04-14 13:53:01 +0200

    Fix database problem
    Fix database problem

---
+++ freditor-database/run-initialization.sh
@@ -0,0 +1,3 @@
+# Run the setup script to create the DB and the schema in the DB
+# Note: make sure that your password matches what is in the Dockerfile
+/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P usertest@12 -d master -i freditorscript.sql
