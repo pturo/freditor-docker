6008416ad4c6741566e10a71229923153a30da0e
tdevelop12 <pawel.turon1@gmail.com>
2022-05-24 11:52:51 +0200

    Add AuthGuard to provider's array
    Add AuthGuard to provider's array

--- freditor-frontend/src/app/app-routing.module.ts
+++ freditor-frontend/src/app/app-routing.module.ts
@@ -35,6 +35,7 @@ const routes: Routes = [
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, { useHash: false })],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: [AuthGuard]
 })
 export class AppRoutingModule { }
